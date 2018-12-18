2654fdb46e592b8520e9cbc002635b57e149fef1
yuzheng <yuzheng1987@sina.com>
2018-12-18 16:12:00 +0800

    laravel-admin框架

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'find'),
+            'database' => env('DB_DATABASE', 'findpro'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', '1234!QAZ@WSX'),
             'unix_socket' => env('DB_SOCKET', ''),
